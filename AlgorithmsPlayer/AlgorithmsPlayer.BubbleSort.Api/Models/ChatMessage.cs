dd9a73574932c398f977b76cc683986b990c6532
altair961 <altair961@users.noreply.github.com>
2019-02-02 19:58:09 +0300

    Added ChatMessages model

---
+++ AlgorithmsPlayer/AlgorithmsPlayer.BubbleSort.Api/Models/ChatMessage.cs
@@ -0,0 +1,9 @@
+namespace AlgorithmsPlayer.BubbleSort.Api
+{
+    public class ChatMessage
+    {
+		public string User { get; set; }
+		public string Message { get; set; }
+		public string Room { get; set; }
+	}
+}
